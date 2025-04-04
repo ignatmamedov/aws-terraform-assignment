1b2a4c839d9a8200f3fce9edbe108bf33fa51337
Ilarion Petriv <115731701+larik-22@users.noreply.github.com>
2025-04-04 21:56:24 +0200

    Main.tf adjutments

--- infra/main.tf
+++ infra/main.tf
@@ -71,7 +71,7 @@ resource "aws_security_group" "exam_sg" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    security_groups = [aws_security_group.exam_sg.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
